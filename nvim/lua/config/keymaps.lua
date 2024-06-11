de8502f8ffb960769e61a51954f0d3f0680382dd
Cdric_Art <cdric.art@gmail.com>
2024-06-11 12:56:57 +0200

    delete config nvim

--- nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps hereby
-vim.keymap.set("n", "<leader>n", ":Neotree filesystem focus<CR>", {})
-vim.keymap.set("n", "<tab>", ":BufferLineCycleNext<CR>")
